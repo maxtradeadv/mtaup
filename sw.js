0610c53cac748f966431abd70c5699dc7d537060
maxtradeadv <maxsave.shop@gmail.com>
2026-09-22 23:18:55 +0700

    Bump service worker cache for scanner update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stock-flow-v29';
+const CACHE='stock-flow-v30';
 const ASSETS=['./','./index.html','./styles.css','./analysis.js','./data-provider.js','./calibration.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
