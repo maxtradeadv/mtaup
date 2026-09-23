82a7a5fbbec335492bd35ac7fa623e058b70ec4f
maxtradeadv <maxsave.shop@gmail.com>
2026-09-23 08:53:46 +0700

    Bump cache for IDX source

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stock-flow-v32';
+const CACHE='stock-flow-v33';
 const ASSETS=['./','./index.html','./styles.css','./analysis.js','./data-provider.js','./calibration.js','./app.js','./manifest.webmanifest','./icon.svg','./icon-180.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
